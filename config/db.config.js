c7fcd9341585237f1583b6079d1902216a91f5b5
dev-gamer <dev29.anurag@gmail.com>
2022-09-07 02:56:51 +0530

    db

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,11 @@ const db = require('mysql2')
 const connection = db.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Beat@@#$1234#123456789',
     database: 'beatnik'
 })
 
+// 'Beat@@#$1234#123456789'
 
 connection.connect(function (err) {
     if (err) throw err
